2bcf63908030d2537fca2a6622e8f54fe6b7cc76
Jer <jeremy@boldapps.net>
2017-06-28 18:50:48 -0500

    finalize new build process

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ babel src/FadeModal.js --out-file FadeModal.js
 babel src/FlyModal.js --out-file FlyModal.js
 babel src/modalFactory.js --out-file modalFactory.js
 babel src/OutlineModal.js --out-file OutlineModal.js
+babel src/ScaleModal.js --out-file ScaleModal.js
 babel src/WaveModal.js --out-file WaveModal.js
